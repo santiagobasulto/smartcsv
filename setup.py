2733b5d20e429f2ac64fd2becff082f0951b76af
Santiago Basulto <santiago.basulto@gmail.com>
2014-08-25 11:54:34 -0300

    Bumped version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(TestCommand):
 
 setup(
     name='smartcsv',
-    version='0.1',
+    version='0.1.1',
     description="CSVs are awesome, yet they're pretty dumb. Let's get them smarter!",
     url='http://github.com/santiagobasulto/smartcsv',
     author='Santiago Basulto',
